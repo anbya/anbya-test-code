f73a23c68e7fcbbdeeab390698d34ffeeaa80a73
anbya <anbyaalibia@gmail.com>
2019-09-05 15:22:20 +0700

    a

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 $('#inputFileButton').on('click', function() {
     $('#inputFile').trigger('click');
 });
-// $(document).ready(function() { 
-//     $("#inputFile").change(function(e) { 
-//         let filepict1 = document.getElementById("inputFile").files[0];
-//         document.getElementById("previewPict").src = filepict1;
-//     }); 
-// });
 $("#inputFile").change(function(){
-    bacaGambar(this);
+    validasiFile(this);
  });
+ function validasiFile(input){
+    let namaFile = input.files[0].name;
+    let namaFileAfterSplit=namaFile.split(".");
+    let cekEkstention = namaFileAfterSplit[1];
+    if(cekEkstention == "PNG" || cekEkstention == "png" || cekEkstention == "jpg" || cekEkstention == "JPG"|| cekEkstention == "jpeg"|| cekEkstention == "JPEG"){
+        bacaGambar(input);
+    }
+    else{
+        alert("file tidak dapat di upload");
+        document.getElementById("inputFile").value = "";
+        $('#previewPict').attr('src', './assets/images/defaultImage.png');
+    }
+}
 function bacaGambar(input) {
     if (input.files && input.files[0]) {
        var reader = new FileReader();
